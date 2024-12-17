00cba6090618fa6e586115d59fc9d8b034590c41
gabriel-psr <gabriel.perez@rackspace.com>
2024-12-17 13:17:15 -0600

    Refactor to use variables 3

--- main.tf
+++ main.tf
@@ -23,10 +23,10 @@ module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.environment.name
-  cidr = "$(var.environment.network_prefix).0.0/16"
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["$(var.environment.network_prefix).101.0/24", "$(var.environment.network_prefix).102.0/24", "$(var.environment.network_prefix).103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
   
   tags = {
 
@@ -41,12 +41,12 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "$(var.environment.name)-blog"
+  name = "${var.environment.name}-blog"
 
   min_size = var.asg_min_size
   max_size = var.asg_max_size
 
-  vpc_zone_identifier = module.blog_vpc.public_subnets
+  vpc_zone_identifier  = module.blog_vpc.public_subnets
   target_group_arns    = module.blog_alb.target_group_arns
   security_groups      = [module.blog_sg.security_group_id]
 
@@ -68,7 +68,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "$(var.environment.name)"
+      name_prefix      = "${var.environment.name}"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -93,7 +93,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
  
-  name                = "$(var.environment.name)-blog"
+  name                = "${var.environment.name}-blog"
   vpc_id              = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
